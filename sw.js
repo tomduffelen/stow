eee1154181b365baff0d02ab5d7fd23a956efa3b
tomduffelen <110622880+tomduffelen@users.noreply.github.com>
2026-09-06 11:23:37 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'stow-v3';
+const CACHE = 'stow-v5';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
